27baa53691592d0076897d9da87baa5572ddc600
Kaan Türkmen <kaanturkmen@protonmail.com>
2022-01-03 23:45:21 +0300

    Create MailConstants.java

---
+++ src/main/java/tr/edu/ku/devnull/needforspear/Viewmodel/MailVerification/MailConstants.java
@@ -0,0 +1,6 @@
+package tr.edu.ku.devnull.needforspear.Viewmodel.MailVerification;
+
+public interface MailConstants {
+    String SENDER_MAIL = "SENDER_MAIL_ADDRESS";
+    String SENDER_MAIL_PASSWORD = "SENDER_MAIL_PASSWORD";
+}
