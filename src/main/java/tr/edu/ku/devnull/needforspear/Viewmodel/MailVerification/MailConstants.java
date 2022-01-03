782b85e3c4e33bd859393848ec943be230716361
Kaan Türkmen <kaanturkmen@protonmail.com>
2022-01-03 23:46:22 +0300

    Update MailConstants.java

--- src/main/java/tr/edu/ku/devnull/needforspear/Viewmodel/MailVerification/MailConstants.java
+++ src/main/java/tr/edu/ku/devnull/needforspear/Viewmodel/MailVerification/MailConstants.java
@@ -1,5 +1,8 @@
 package tr.edu.ku.devnull.needforspear.Viewmodel.MailVerification;
 
+/**
+ * MailConstants file is created to access game mail credentials to send verification mails to the users.
+ */
 public interface MailConstants {
     String SENDER_MAIL = "SENDER_MAIL_ADDRESS";
     String SENDER_MAIL_PASSWORD = "SENDER_MAIL_PASSWORD";
