1e2583388bf6f9d738370d227afedf347f3624e4
Kaan Türkmen <kaanturkmen@protonmail.com>
2022-01-03 22:44:42 +0300

    Restoring the deleted fields.

--- src/main/java/tr/edu/ku/devnull/needforspear/View/AuthViews/ActivationView.java
+++ src/main/java/tr/edu/ku/devnull/needforspear/View/AuthViews/ActivationView.java
@@ -47,6 +47,7 @@ public class ActivationView {
     private void determineUIElementsSizes() {
         int x_coordinates_loc = (int) Constants.UIConstants.AUTH_VIEW_EXCEPT_LOGIN_LOCATION.getXCoordinates();
         int y_coordinates_loc = (int) Constants.UIConstants.AUTH_VIEW_EXCEPT_LOGIN_LOCATION.getYCoordinates();emailField.setBounds(x_coordinates_loc, y_coordinates_loc + 2 * Constants.UIConstants.PADDING_BETWEEN_TEXT_FIELDS, Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getWidth(), Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getLength());
+        emailField.setBounds(x_coordinates_loc, y_coordinates_loc + 2 * Constants.UIConstants.PADDING_BETWEEN_TEXT_FIELDS, Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getWidth(), Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getLength());
         codeField.setBounds(x_coordinates_loc, y_coordinates_loc + 3 * Constants.UIConstants.PADDING_BETWEEN_TEXT_FIELDS, Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getWidth(), Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getLength());
         activateButton.setBounds(x_coordinates_loc, y_coordinates_loc + 4 * Constants.UIConstants.PADDING_BETWEEN_TEXT_FIELDS, Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getWidth(), Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getLength());
         backButton.setBounds(Constants.UIConstants.BACK_BUTTON_X_COORDINATE, Constants.UIConstants.BACK_BUTTON_Y_COORDINATE, Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getWidth(), Constants.UIConstants.MENU_AND_AUTH_VIEW_COMPONENT_SIZE.getLength());
@@ -79,7 +80,8 @@ public class ActivationView {
         NeedforSpearGame.getInstance().getGameInfo().getMainFrame().add(emailField);
         NeedforSpearGame.getInstance().getGameInfo().getMainFrame().add(codeField);
         NeedforSpearGame.getInstance().getGameInfo().getMainFrame().add(activateButton);
-        NeedforSpearGame.getInstance().getGameInfo().getMainFrame().add(backButton);NeedforSpearGame.getInstance().getGameInfo().getMainFrame().setLayout(null);
+        NeedforSpearGame.getInstance().getGameInfo().getMainFrame().add(backButton);
+        NeedforSpearGame.getInstance().getGameInfo().getMainFrame().setLayout(null);
         NeedforSpearGame.getInstance().getGameInfo().getMainFrame().setVisible(true);
     }
 }
